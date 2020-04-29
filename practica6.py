82157a958d50d3254879f37fe701e3da37f24349
David <davidvalverdeencinas091@gmail.com>
2020-04-29 17:32:34 +0200

    Ejercicio 6 hecho

--- practica6.py
+++ practica6.py
@@ -5,18 +5,17 @@ print("BIENVENIDO A MI CIFRADOR CÉSAR")
 texto_claro=input("Escribe el texto a cifrar: ")
 clave=int(input("Escribe la clave de cifrado (un número del 1 al 27): "))
 
-def cifracesar(texto,key):
-    texto_cifrado=""
-    for letra in texto_claro:
-        nueva_posicion = abecedario.find(letra) + clave
-        letra_cifrada = int(nueva_posicion) % len(abecedario)
-        texto_cifrado = texto_cifrado + str(abecedario[letra_cifrada])
-    print("\nEl mensaje cifrado es: ",texto_cifrado)
-    return texto_cifrado
-    texto_descifrado=""
-    for letra in texto_cifrado:
-        nueva_posicion = abecedario.find(letra) - clave
-        letra_cifrada = int(nueva_posicion) % len(abecedario)
-        texto_descifrado = texto_descifrado + str(abecedario[letra_cifrada])
+texto_cifrado=""
+for letra in texto_claro:
+    nueva_posicion = abecedario.find(letra) + clave
+    letra_cifrada = int(nueva_posicion) % len(abecedario)
+    texto_cifrado = texto_cifrado + str(abecedario[letra_cifrada])
+print("\nEl mensaje cifrado es: ",texto_cifrado)
 
-    print("\nEl mensaje descifrado es: ",texto_descifrado)
+texto_descifrado=""
+for letra in texto_cifrado:
+    nueva_posicion = abecedario.find(letra) - clave
+    letra_cifrada = int(nueva_posicion) % len(abecedario)
+    texto_descifrado = texto_descifrado + str(abecedario[letra_cifrada])
+
+print("\nEl mensaje descifrado es: ",texto_descifrado)
